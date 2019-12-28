ac3e582874baf45da17de27d58dd3e03082e48b0
RUCHON <bastien.ruchon81@gmail.com>
2019-12-28 10:02:38 +0100

    Update eslinter, update rules to have personal configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,8 +10,15 @@ module.exports = {
     'react/prop-types': 'off',
     'comma-dangle': 'off',
     "linebreak-style": 0,
-    "indent": ["error", 2],
-    "react/jsx-indent": ["error", 4]
+    "indent": ["error", 4],
+    "react/jsx-indent": ["error", 4],
+    "react/jsx-indent-props": ["error", 4],
+    "object-curly-newline": ["error", {
+      "ObjectExpression": "always",
+      "ObjectPattern": { "multiline": true },
+      "ImportDeclaration": "never",
+      "ExportDeclaration": { "multiline": true, "minProperties": 3 }
+    }]
   },
   'globals': {
     "fetch": false
