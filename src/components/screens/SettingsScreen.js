6b4a6b01c58b650b17bd1519b5bc6bd66dc8e57d
RUCHON <bastien.ruchon81@gmail.com>
2020-01-03 11:35:40 +0100

    Init webpack and config files

--- src/components/screens/SettingsScreen.js
+++ src/components/screens/SettingsScreen.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 
 import Menu from './Menu';
@@ -7,7 +7,7 @@ const SettingsScreen = () => {
 
     return (
         <>
-            <Menu page={'Settings'}></Menu>
+            <Menu page="Settings" />
             <Text>Settings Land</Text>
         </>
     );
