1ffe129f6a9d189ba35518ed090038548b7ca464
Bastien <bastien.ruchon81@gmail.com>
2020-01-05 11:27:46 +0100

    Update babel config

--- babel.config.js
+++ babel.config.js
@@ -4,17 +4,17 @@ module.exports = function(api) {
     presets: ['babel-preset-expo'],
     plugins: [
       [
-        "module-resolver",
+        'module-resolver',
         {
           "root": ["./"],
-          "alias": {
-            "@components": "./src/components",
-            "@organisms": "./src/components/organisms",
-            "@screens": "./src/components/screens",
-            "@stores": "./src/stores",
-            "@utils": "./src/utils",
-            "@services": "./src/services",
-            "@assets": "./assets",
+          'alias': {
+            '@components': './src/components',
+            '@organisms': './src/components/organisms',
+            '@screens': './src/components/screens',
+            '@stores': './src/stores',
+            '@utils': './src/utils',
+            '@services': './src/services',
+            '@assets': './assets',
           }
         },
       ],
--- src/App.js
+++ src/App.js
@@ -3,14 +3,8 @@ import { registerRootComponent } from 'expo';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 
-// import HomeScreen from 'HomeScreen';
-
 import HomeScreen from '@screens/HomeScreen';
-
-// import SoundScreen from './SoundScreen';
 import SettingsScreen from '@screens/SettingsScreen';
-
-// import { SettingsScreen } from 'Screens/SettingsScreen.js';
 
 const MainNavigator = createStackNavigator({
   Home: { screen: HomeScreen },
